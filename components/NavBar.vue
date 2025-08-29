e753344f17a8d10ce0e485c0f81be759d0858781
Aurore Ducobu <aurore@MacBook-Air-de-Aurore-15.local>
2025-08-29 18:40:31 +0200

    init dev environment

---
+++ components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="nav-bar">
+    <router-link to="/">Acceuil</router-link>
+    <router-link to="/course">Parcours</router-link>
+    <router-link to="/registration">Inscription</router-link>
+    <router-link to="/results">Résultats</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
